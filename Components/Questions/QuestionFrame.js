21d00a48745b90433e992b0e322ea5ca2ffb0dc9
Nathan Luu <nate1299@me.com>
2020-06-20 12:01:00 -0700

    Created componens and pages + uploaded logos

---
+++ Components/Questions/QuestionFrame.js
@@ -0,0 +1,9 @@
+export default function ({question}) {
+    return (
+        <article>
+            <p>{question.text}</p>
+
+
+    </article>)
+
+}
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import Head from 'next/head'
 import Navbar from "../Components/Navbar";
 
 export default function Home() {
+    const navBarItems = [{icon: 'fas fa-circle', label: 'Features'}, {icon: 'fas fa-circle', label: 'About us'}, {icon: 'fas fa-circle', label: 'Pricing'}];
     return (
         <>
-            <Navbar/>
-            <div className="max-w-5xl mx-auto">
+            <Navbar items={navBarItems} exam/>
+            <div className="max-w-5xl mx-auto pt-32 px-4">
                 <header>
-                    <h1 className="text-7xl text-center font-black text-gray-900">Online assignments that just work</h1>
+                    <h1 className="text-5xl md:text-6xl lg:text-7xl text-center font-black text-gray-900">Online assignments that just work</h1>
                 </header>
             </div>
         </>
