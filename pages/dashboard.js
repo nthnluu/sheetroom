308dd7054313dfc3459cef575a110f52de45d178
Nathan Luu <nate1299@me.com>
2020-06-26 22:04:49 -0700

    Added frame for dashboard page

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,16 +1,160 @@
-import Navbar from "../Components/Navbar/Navbar";
-import UserContext from "../Components/AuthProvider";
-import React, {useContext} from 'react';
-import AuthenticatedNavbar from "../Components/Navbar/AuthenticatedNavbar";
-import {useQuery} from "@apollo/react-hooks";
-import {ME} from "../gql/authentication";
+import React, {useState} from 'react';
+import auth0 from "../utils/auth0";
+import Transition from "../Components/Transition";
 
-const Index = () => {
-    const user = useContext(UserContext);
-    const {loading, error, data} = useQuery(ME, {variables: {token: user}});
-    if (loading) return '';
-    if (error) return `Error! ${error.message}`;
+function AuthNavbar({user}) {
+    const [profileDropdown, toggleProfileDropdown] = useState(false);
 
+    return (<nav className="bg-white border-b border-gray-200">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between h-16">
+                <div className="flex">
+                    <div className="flex-shrink-0 flex items-center">
+                        <img className="block lg:hidden h-8 w-auto"
+                             src="/hw_symbol.svg" alt="Workflow logo"/>
+                        <img className="hidden lg:block h-8 w-auto"
+                             src="/hw_logo.svg" alt="Workflow logo"/>
+                    </div>
+                    <div className="hidden sm:-my-px sm:ml-6 sm:flex">
+                        <a href="#"
+                           className="inline-flex items-center px-1 pt-1 border-b-2 border-blue-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-blue-700 transition duration-150 ease-in-out">
+                            Dashboard
+                        </a>
+                        <a href="#"
+                           className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            Team
+                        </a>
+                        <a href="#"
+                           className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            Projects
+                        </a>
+                        <a href="#"
+                           className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            Calendar
+                        </a>
+                    </div>
+                </div>
+                <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                    <button
+                        className="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:text-gray-500 focus:bg-gray-100 transition duration-150 ease-in-out"
+                        aria-label="Notifications">
+                        <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
+                        </svg>
+                    </button>
+
+                    {/*// <!-- Profile dropdown -->*/}
+                    <div className="ml-3 relative">
+                        <div>
+                            <button onClick={() => toggleProfileDropdown(!profileDropdown)}
+                                className="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out"
+                                id="user-menu" aria-label="User menu" aria-haspopup="true">
+                                <img className="h-8 w-8 rounded-full"
+                                     src={user.picture}
+                                     alt=""/>
+                            </button>
+                        </div>
+                        <Transition show={profileDropdown} enter="transition ease-out duration-200"
+                                    enterFrom="transform opacity-0 scale-95"
+                                    enterTo="transform opacity-100 scale-100"
+                                    leave="transition ease-in duration-75"
+                                    leaveFrom="transform opacity-100 scale-100"
+                                    leaveTo="transform opacity-0 scale-95">
+                            <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
+                                <div className="py-1 rounded-md bg-white shadow-xs">
+                                    <a href="#"
+                                       className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Your
+                                        Profile
+                                    </a>
+                                    <a href="#"
+                                       className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Settings
+                                    </a>
+                                    <a href="/api/logout"
+                                       className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Sign
+                                        out
+                                    </a>
+                                </div>
+                            </div>
+                        </Transition>
+                    </div>
+                </div>
+                <div className="-mr-2 flex items-center sm:hidden">
+                    {/*// <!-- Mobile menu button -->*/}
+                    <button
+                        className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                        {/*// <!-- Menu open: "hidden", Menu closed: "block" -->*/}
+                        <svg className="block h-6 w-6" stroke="currentColor" fill="none"
+                             viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M4 6h16M4 12h16M4 18h16"/>
+                        </svg>
+                        {/*// <!-- Menu open: "block", Menu closed: "hidden" -->*/}
+                        <svg className="hidden h-6 w-6" stroke="currentColor" fill="none"
+                             viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M6 18L18 6M6 6l12 12"/>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        {/*// <!--*/}
+        {/*//   Mobile menu, toggle classes based on menu state.*/}
+        {/*//*/}
+        {/*//   Open: "block", closed: "hidden"*/}
+        {/*// -->*/}
+        <div className="block sm:hidden">
+            <div className="pt-2 pb-3">
+                <a href="#"
+                   className="block pl-3 pr-4 py-2 border-l-4 border-blue-500 text-base font-medium text-blue-700 bg-blue-50 focus:outline-none focus:text-blue-800 focus:bg-blue-100 focus:border-blue-700 transition duration-150 ease-in-out">Dashboard
+                </a>
+                <a href="#"
+                   className="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out">Team
+                </a>
+                <a href="#"
+                   className="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out">Projects
+                </a>
+                <a href="#"
+                   className="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out">Calendar
+                </a>
+            </div>
+            <div className="pt-4 pb-3 border-t border-gray-200">
+                <div className="flex items-center px-4">
+                    <div className="flex-shrink-0">
+                        <img className="h-10 w-10 rounded-full"
+                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                             alt=""/>
+                    </div>
+                    <div className="ml-3">
+                        <div className="text-base font-medium leading-6 text-gray-800">{user.given_name}
+                        </div>
+                        <div className="text-sm font-medium leading-5 text-gray-500">tom@example.com
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-3" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+                    <a href="#"
+                       className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out"
+                       role="menuitem">Your Profile
+                    </a>
+                    <a href="#"
+                       className="mt-1 block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out"
+                       role="menuitem">Settings
+                    </a>
+                    <a href="#"
+                       className="mt-1 block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out"
+                       role="menuitem">Sign out
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>)
+
+}
+
+const Dashboard = ({user}) => {
     const navBarItems = {
         links: [{label: 'Library'}, {label: 'Your Classes'}, {label: 'More'}],
         actionButtons: {primary: {label: 'Sign up', href: '/signup'}, secondary: {label: 'Log in', href: '/login'}}
@@ -18,35 +162,45 @@ const Index = () => {
 
     return (
         <>
-            <AuthenticatedNavbar items={navBarItems.links} actionButtons={navBarItems.actionButtons} token={user}/>
-            <div className="max-w-6xl mx-auto pt-16 px-4 xl:px-0">
-                <header>
-                    <div className="md:flex md:items-center md:justify-between">
-                        <div className="flex-1 min-w-0">
-                            <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
-                                Welcome back,{" " + data.me.firstName}
-                            </h2>
+            <div className="min-h-screen bg-white">
+                <AuthNavbar user={user}/>
+                <div className="py-10">
+                    <header>
+                        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                            <h1 className="text-3xl font-bold leading-tight text-gray-900">
+                                Dashboard
+                            </h1>
                         </div>
-                        <div className="mt-4 flex md:mt-0 md:ml-4">
-                            <span className="shadow-sm rounded-md">
-                                <button type="button"
-                                        className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 transition duration-150 ease-in-out">
-                                    Edit
-                                </button>
-                            </span>
-                            <span className="ml-3 shadow-sm rounded-md">
-                                <button type="button"
-                                        className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:shadow-outline-purple focus:border-gray-700 active:bg-gray-700 transition duration-150 ease-in-out">
-                                    New Assignment
-                                </button>
-                            </span>
+                    </header>
+                    <main>
+                        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                            {/*// <!-- Replace with your content -->*/}
+                            <div className="px-4 py-8 sm:px-0">
+                                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+                            </div>
+                            {/*// <!-- /End replace -->*/}
                         </div>
-                    </div>
-                </header>
+                    </main>
+                </div>
             </div>
         </>
     )
 };
 
-// export default withApollo({ ssr: true })(Index);
-export default Index;
+
+Dashboard.getInitialProps = async ({req, res}) => {
+    if (typeof window === 'undefined') {
+        const session = await auth0.getSession(req);
+        if (!session || !session.user) {
+            res.writeHead(302, {
+                Location: '/api/login'
+            });
+            res.end();
+            return;
+        }
+        return {user: session.user};
+    }
+};
+
+
+export default Dashboard;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Navbar from "../Components/Navbar/Navbar";
 import React from 'react';
-import auth0 from "../utils/auth0";
 
-const Index = ({user}) => {
+const Index = () => {
     const navBarItems = {
         links: [{label: 'Features'}, {label: 'About us'}, {label: 'Pricing'}],
         actionButtons: {primary: {label: 'Sign up', href: '/api/login'}, secondary: {label: 'Log in', href: '/api/login'}}
@@ -16,26 +15,10 @@ const Index = ({user}) => {
                     <h1 className="text-5xl md:text-6xl lg:text-7xl text-center font-black text-gray-900">Online
                         assignments that just work
                     </h1>
-                    <a href="/api/login">Login</a>
                 </header>
             </div>
         </>
     )
-};
-
-
-Index.getInitialProps = async ({ req, res }) => {
-    if (typeof window === 'undefined') {
-        const session = await auth0.getSession(req);
-        if (!session || !session.user) {
-            res.writeHead(302, {
-                Location: '/api/login'
-            });
-            res.end();
-            return;
-        }
-        return { user: session.user };
-    }
 };
 
 // export default withApollo({ ssr: true })(Index);
