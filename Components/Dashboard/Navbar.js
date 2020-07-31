3c9eb5cd33b3cd688b740cc7ec9af2e3d6a8399a
Nathan Luu <nate1299@me.com>
2020-07-31 16:56:58 -0700

    Updated navbar colors

--- Components/Dashboard/Navbar.js
+++ Components/Dashboard/Navbar.js
@@ -3,7 +3,7 @@ import Transition from "../Transition";
 
 const Navbar = () => {
     const [profileDropdown, toggleProfileDropdown] = useState(false);
-    return <nav className="flex-shrink-0 bg-gray-800">
+    return <nav className="flex-shrink-0 bg-gray-900">
         <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
                 {/* Logo section */}
@@ -19,7 +19,7 @@ const Navbar = () => {
                 <div className="flex-1 flex justify-center lg:justify-end">
                     <div className="w-full px-2 lg:px-6">
                         <label htmlFor="search" className="sr-only">Search assignment</label>
-                        <div className="relative text-blue-300 focus-within:text-gray-400">
+                        <div className="relative text-gray-300 focus-within:text-gray-400">
                             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                 <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
                                     <path fillRule="evenodd"
@@ -28,7 +28,7 @@ const Navbar = () => {
                                 </svg>
                             </div>
                             <input id="search"
-                                   className="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-blue-400 bg-opacity-25 text-blue-300 placeholder-blue-300 focus:outline-none focus:bg-white focus:placeholder-gray-400 focus:text-gray-900 sm:text-sm transition duration-150 ease-in-out"
+                                   className="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-gray-400 bg-opacity-25 text-gray-300 placeholder-gray-300 focus:outline-none focus:bg-white focus:placeholder-gray-400 focus:text-gray-900 sm:text-sm transition duration-150 ease-in-out"
                                    placeholder="Search assignments" type="search"/>
                         </div>
                     </div>
@@ -36,7 +36,7 @@ const Navbar = () => {
                 <div className="flex lg:hidden">
                     {/* Mobile menu button */}
                     <button
-                        className="inline-flex items-center justify-center p-2 rounded-md text-blue-400 hover:text-white hover:bg-blue-600 focus:outline-none focus:bg-blue-600 focus:text-white transition duration-150 ease-in-out"
+                        className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-600 focus:outline-none focus:bg-gray-600 focus:text-white transition duration-150 ease-in-out"
                         aria-label="Main menu" aria-expanded="false">
                         {/* Icon when menu is closed. */}
                         {/* Menu open: "hidden", Menu closed: "block" */}
@@ -57,9 +57,9 @@ const Navbar = () => {
                     <div className="flex items-center justify-end">
                         <div className="flex">
                             <a href="#"
-                               className="px-3 py-2 rounded-md text-sm leading-5 font-medium text-blue-200 hover:text-white focus:outline-none focus:text-white focus:bg-blue-600 transition duration-150 ease-in-out">Documentation</a>
+                               className="px-3 py-2 rounded-md text-sm leading-5 font-medium text-gray-200 hover:text-white focus:outline-none focus:text-white focus:bg-gray-600 transition duration-150 ease-in-out">Documentation</a>
                             <a href="#"
-                               className="px-3 py-2 rounded-md text-sm leading-5 font-medium text-blue-200 hover:text-white focus:outline-none focus:text-white focus:bg-blue-600 transition duration-150 ease-in-out">Support</a>
+                               className="px-3 py-2 rounded-md text-sm leading-5 font-medium text-gray-200 hover:text-white focus:outline-none focus:text-white focus:bg-gray-600 transition duration-150 ease-in-out">Support</a>
                         </div>
                         {/* Profile dropdown */}
 
@@ -108,19 +108,19 @@ const Navbar = () => {
         <div className="hidden lg:hidden">
             <div className="px-2 pt-2 pb-3">
                 <a href="#"
-                   className="block px-3 py-2 rounded-md text-base font-medium text-white bg-blue-800 focus:outline-none focus:text-blue-100 focus:bg-blue-600 transition duration-150 ease-in-out">Dashboard</a>
+                   className="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-800 focus:outline-none focus:text-gray-100 focus:bg-gray-600 transition duration-150 ease-in-out">Dashboard</a>
                 <a href="#"
-                   className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-blue-200 hover:text-blue-100 hover:bg-blue-600 focus:outline-none focus:text-white focus:bg-blue-600 transition duration-150 ease-in-out">Support</a>
+                   className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-200 hover:text-gray-100 hover:bg-gray-600 focus:outline-none focus:text-white focus:bg-gray-600 transition duration-150 ease-in-out">Support</a>
             </div>
-            <div className="pt-4 pb-3 border-t border-blue-800">
+            <div className="pt-4 pb-3 border-t border-gray-800">
                 <div className="mt-3 px-2">
                     <a href="#"
-                       className="block px-3 py-2 rounded-md text-base font-medium text-blue-200 hover:text-blue-100 hover:bg-blue-600 focus:outline-none focus:text-white focus:bg-blue-600 transition duration-150 ease-in-out">Your
+                       className="block px-3 py-2 rounded-md text-base font-medium text-gray-200 hover:text-gray-100 hover:bg-gray-600 focus:outline-none focus:text-white focus:bg-gray-600 transition duration-150 ease-in-out">Your
                         Profile</a>
                     <a href="#"
-                       className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-blue-200 hover:text-blue-100 hover:bg-blue-600 focus:outline-none focus:text-white focus:bg-blue-600 transition duration-150 ease-in-out">Settings</a>
+                       className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-200 hover:text-gray-100 hover:bg-gray-600 focus:outline-none focus:text-white focus:bg-gray-600 transition duration-150 ease-in-out">Settings</a>
                     <a href="#"
-                       className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-blue-200 hover:text-blue-100 hover:bg-blue-600 focus:outline-none focus:text-white focus:bg-blue-600 transition duration-150 ease-in-out">Sign
+                       className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-200 hover:text-gray-100 hover:bg-gray-600 focus:outline-none focus:text-white focus:bg-gray-600 transition duration-150 ease-in-out">Sign
                         out</a>
                 </div>
             </div>
