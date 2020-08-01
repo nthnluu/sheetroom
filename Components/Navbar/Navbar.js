f654525581bb3d7577fc7bfc1e9a059b5fa1614f
Nathan Luu <nate1299@me.com>
2020-07-31 18:49:57 -0700

    Added new logos

--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ export default function () {
     return (
         // <!-- This example requires Tailwind CSS v1.4.0+ -->
         <div className="relative bg-white">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6">
+            <div className="mx-auto px-4 sm:px-6">
                 <div
                     className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
                     <div className="lg:w-0 lg:flex-1">
