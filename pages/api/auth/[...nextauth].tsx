da7088b8e3200a1da1b149399f87707b68c0978f
Nathan Luu <nate1299@me.com>
2020-08-03 16:28:40 -0700

    added ssl fron githunb

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -23,19 +23,22 @@ const options = {
             clientSecret: "55505558a1d8785347d8d02e9cb5eeeb86f616fd"
         })
     ],
-    database: "postgres://rkofrjdyqoidnj:5e700ce4e559ae08a4306f70d66e203c9d6933b4afa5990f5766f31b26666c85@ec2-52-72-65-76.compute-1.amazonaws.com:5432/d2rnd6jboqu0mq"+"?sslmode=require",
+    // database: "postgres://rkofrjdyqoidnj:5e700ce4e559ae08a4306f70d66e203c9d6933b4afa5990f5766f31b26666c85@ec2-52-72-65-76.compute-1.amazonaws.com:5432/d2rnd6jboqu0mq"+"?sslmode=require",
 
     // A database is optional, but required to persist accounts in a database
-    // database: {
-    //     type: 'postgres',
-    //     host: "ec2-52-72-65-76.compute-1.amazonaws.com",
-    //     port: 5432,
-    //     username: "rkofrjdyqoidnj",
-    //     password: "5e700ce4e559ae08a4306f70d66e203c9d6933b4afa5990f5766f31b26666c85",
-    //     database: "d2rnd6jboqu0mq",
-    //     synchronize: false,
-    //     ssl: true
-    // },
+    database: {
+        type: 'postgres',
+        host: "ec2-52-72-65-76.compute-1.amazonaws.com",
+        port: 5432,
+        username: "rkofrjdyqoidnj",
+        password: "5e700ce4e559ae08a4306f70d66e203c9d6933b4afa5990f5766f31b26666c85",
+        database: "d2rnd6jboqu0mq",
+        synchronize: false,
+        ssl: {
+            rejectUnauthorized: false,
+            ca: fs.readFileSync(certFile).toString(),
+        },
+    },
     callbacks: {
         session: async (session: ISession, user: IUser) => {
             session.id = user.id;
