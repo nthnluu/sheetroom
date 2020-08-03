0b4650410f14a40401fa31edd06583ed2fc11a91
Nathan Luu <nate1299@me.com>
2020-08-03 09:54:31 -0700

    updatd ourge css

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ const Index: React.FC = () => {
             <Navbar/>
             <div className="max-w-6xl mx-auto pt-32 px-4">
                 <header>
+                    <h1 className="text-5xl md:text-6xl lg:text-7xl text-center font-black text-gray-900 leading-tight" style={{fontSize: '12rem'}}>Build
+                    </h1>
                     <h1 className="text-5xl md:text-6xl lg:text-7xl text-center font-black text-gray-900 leading-tight">Beautiful
                         assignments for desktop, mobile and paper.
                     </h1>
