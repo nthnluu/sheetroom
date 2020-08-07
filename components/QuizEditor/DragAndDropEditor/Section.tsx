3a3048cac9bdaaca7447785dde4d61b2920162ea
Nathan Luu <nate1299@me.com>
2020-08-07 15:27:30 -0700

    Added logic to insert between

--- components/QuizEditor/DragAndDropEditor/Section.tsx
+++ components/QuizEditor/DragAndDropEditor/Section.tsx
@@ -10,7 +10,7 @@ interface Props {
 }
 
 const Section: React.FC<Props> = ({section, index}) => {
-    const {setDocument} = useContext(QuizContext)
+    const {setDocument, document} = useContext(QuizContext)
     const [isCollapsed, toggleIsCollapsed] = useState(false);
 
     const addMcItem = () => {
@@ -48,8 +48,51 @@ const Section: React.FC<Props> = ({section, index}) => {
         })
     }
 
+    const addSection = () => {
+
+        setDocument(prevState => {
+            const newSectionId = uuidv4()
+            const newId = uuidv4()
+            const newObjectId = uuidv4()
+            const newData = update(prevState, {
+                config: {
+                    sections: {
+                        $splice: [[index + 1, 0, newSectionId]]
+                    }
+
+                },
+                    items: {
+                        [newId]: {
+                            $set: {
+                                content: "<p>Option</p>",
+                                controller_type: "MC",
+                                answer_objects: [newObjectId],
+                                correct_objects: [newObjectId]
+                            }
+                        }
+                    }, sections: {
+                        [newSectionId]: {
+                            $set: {
+                                title: prevState.sections.length + 1,
+                                items: [newId]
+                            }
+                        }
+                    },
+                    answer_objects: {
+                        [newObjectId]: {
+                            $set: {
+                                content: "<p><br/></p>"
+                            }
+                        }
+                    }
+                }
+            )
+            return newData
+        })
+    }
+
     return (
-        <div className="mb-8">
+        <div className="mb-8 group">
             <div className="mb-4">
                 <h1 className="text-2xl text-gray-800 font-semibold ">Section {index + 1}<span
                     className="text-gray-300 mx-2">
@@ -69,11 +112,14 @@ const Section: React.FC<Props> = ({section, index}) => {
 
 
             </div>
-            {!isCollapsed ? <ItemDnd section={section}/> : null}
+            {!isCollapsed ? <ItemDnd section={section}/> : <button onClick={() => toggleIsCollapsed(false)}
+                                                                   className="bg-white border border-gray-300 px-4 py-4 rounded-lg focus:outline-none w-full text-left">
+                <h2 className="text-lg font-semibold text-gray-800">{document.sections[section].items.length} items</h2>
+            </button>}
 
-            <div className="flex justify-between mt-4">
-                <button type="button" onClick={() => addMcItem()}
-                        className="inline-flex items-center border-gray-300 text-lg leading-4 font-medium rounded-md text-gray-500 bg-transparent hover:text-gray-400 focus:outline-none focus:border-blue-400 focus:shadow-outline-blue active:text-gray-300 transition ease-in-out duration-150">
+            <div className="flex justify-between mt-4 invisible group-hover:visible ">
+                <button type="button" onClick={() => addSection()}
+                        className="inline-flex items-center border-gray-300 text-lg leading-4 font-medium rounded-md text-gray-300 bg-transparent hover:text-gray-400 focus:outline-none focus:border-blue-400 focus:shadow-outline-blue active:text-gray-300 transition ease-in-out duration-150">
                     <svg width="32" height="32" viewBox="0 0 24 24" className="mr-2" fill="none"
                          xmlns="http://www.w3.org/2000/svg">
                         <path
