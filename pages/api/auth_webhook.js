d62720f4823731920adc7dd524f7c91c33d3d8b7
Nathan Luu <nate1299@me.com>
2020-08-08 03:03:23 -0700

    mor changes

--- pages/api/auth_webhook.js
+++ pages/api/auth_webhook.js
@@ -1,10 +1,20 @@
 // This is an example of how to read a JSON Web Token from an API route
 import jwt from 'next-auth/jwt'
 
-const secret = process.env.SECRET
-
 export default async (req, res) => {
     const secret = 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw'
     const token = await jwt.getToken({ req, secret })
-    res.send(JSON.stringify(token, null, 2))
+    if (token) {
+        // Signed in
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify({
+            "X-Hasura-User-Id": token["X-Hasura-User-Id"],
+            "X-Hasura-Role": "user",
+        }))
+    } else {
+        // Not Signed in
+        res.status(401)
+    }
+    res.end()
 }
