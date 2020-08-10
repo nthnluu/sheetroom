7070786ab1294121f480f6d4d705d9c39187a89c
Nathan Luu <nate1299@me.com>
2020-08-09 23:07:53 -0700

    Added outlined version of collapse button

--- components/AssignmentEditor/DragAndDropEditor/Section.tsx
+++ components/AssignmentEditor/DragAndDropEditor/Section.tsx
@@ -110,19 +110,8 @@ const Section: React.FC<Props> = ({section, index}) => {
                                     enterNextDelay={500}>
                             <button type="button" onClick={() => toggleIsCollapsed(false)}
                                     className="inline-flex text-center items-center h-8 w-8 border border-transparent text-base leading-6 font-medium rounded-md text-gray-600 bg-transparent hover:bg-gray-100 focus:bg-gray-100 active:bg-gray-200 focus:outline-none focus:shadow-outline transition ease-in-out duration-150">
-                                <svg className="h-6 w-6 mx-auto" viewBox="0 0 20 20" fill="none">
-                                    <path
-                                        d="M3 3C2.44772 3 2 3.44772 2 4C2 4.55228 2.44772 5 3 5H14C14.5523 5 15 4.55228 15 4C15 3.44772 14.5523 3 14 3H3Z"
-                                        fill="currentColor"/>
-                                    <path
-                                        d="M3 7C2.44772 7 2 7.44772 2 8C2 8.55228 2.44772 9 3 9H8C8.55228 9 9 8.55228 9 8C9 7.44772 8.55228 7 8 7H3Z"
-                                        fill="currentColor"/>
-                                    <path
-                                        d="M3 11C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13H7C7.55228 13 8 12.5523 8 12C8 11.4477 7.55228 11 7 11H3Z"
-                                        fill="currentColor"/>
-                                    <path
-                                        d="M13 16C13 16.5523 13.4477 17 14 17C14.5523 17 15 16.5523 15 16L15 10.4142L16.2929 11.7071C16.6834 12.0976 17.3166 12.0976 17.7071 11.7071C18.0976 11.3166 18.0976 10.6834 17.7071 10.2929L14.7071 7.29289C14.5196 7.10536 14.2652 7 14 7C13.7348 7 13.4804 7.10536 13.2929 7.29289L10.2929 10.2929C9.90237 10.6834 9.90237 11.3166 10.2929 11.7071C10.6834 12.0976 11.3166 12.0976 11.7071 11.7071L13 10.4142L13 16Z"
-                                        fill="currentColor"/>
+                                <svg className="h-6 w-6 mx-auto" viewBox="0 0 24 24" fill="none">
+                                    <path d="M3 4H16M3 8H12M3 12H12M17 8V20M17 20L13 16M17 20L21 16" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round"/>
                                 </svg>
                             </button>
                         </NewTooltip>
@@ -131,19 +120,8 @@ const Section: React.FC<Props> = ({section, index}) => {
                                     enterNextDelay={500}>
                             <button type="button" onClick={() => toggleIsCollapsed(true)}
                                     className="inline-flex text-center items-center h-8 w-8 border border-transparent text-base leading-6 font-medium rounded-md text-gray-600 bg-transparent hover:bg-gray-100 focus:bg-gray-100 active:bg-gray-200  focus:outline-none focus:shadow-outline transition ease-in-out duration-150">
-                                <svg className="h-6 w-6 mx-auto" viewBox="0 0 20 20" fill="none">
-                                    <path
-                                        d="M3 3C2.44772 3 2 3.44772 2 4C2 4.55228 2.44772 5 3 5H14C14.5523 5 15 4.55228 15 4C15 3.44772 14.5523 3 14 3H3Z"
-                                        fill="currentColor"/>
-                                    <path
-                                        d="M3 7C2.44772 7 2 7.44772 2 8C2 8.55228 2.44772 9 3 9H10C10.5523 9 11 8.55228 11 8C11 7.44772 10.5523 7 10 7H3Z"
-                                        fill="currentColor"/>
-                                    <path
-                                        d="M3 11C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13H7C7.55228 13 8 12.5523 8 12C8 11.4477 7.55228 11 7 11H3Z"
-                                        fill="currentColor"/>
-                                    <path
-                                        d="M15 8C15 7.44772 14.5523 7 14 7C13.4477 7 13 7.44771 13 8L13 13.5858L11.7071 12.2929C11.3166 11.9024 10.6834 11.9024 10.2929 12.2929C9.90237 12.6834 9.90237 13.3166 10.2929 13.7071L13.2929 16.7071C13.4804 16.8946 13.7348 17 14 17C14.2652 17 14.5196 16.8946 14.7071 16.7071L17.7071 13.7071C18.0976 13.3166 18.0976 12.6834 17.7071 12.2929C17.3166 11.9024 16.6834 11.9024 16.2929 12.2929L15 13.5858L15 8Z"
-                                        fill="currentColor"/>
+                                <svg className="h-6 w-6 mx-auto" viewBox="0 0 24 24" fill="none">
+                                    <path d="M3 4H16M3 8H12M3 12H9M13 12L17 8M17 8L21 12M17 8V20" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round"/>
                                 </svg>
                             </button>
                         </NewTooltip>
@@ -161,7 +139,7 @@ const Section: React.FC<Props> = ({section, index}) => {
                                         strokeLinejoin="round"/>
                                     <path
                                         d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z"
-                                        strokeWidth="2" strokeLinecap="round"
+                                        strokeWidth="1.75" strokeLinecap="round"
                                         className="stroke-current"
                                         strokeLinejoin="round"/>
                                 </svg>
