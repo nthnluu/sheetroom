a2b7a68393996a212eb26ce957b9a7a9cb58fae9
Nathan Luu <nate1299@me.com>
2020-08-10 20:24:01 -0700

    added shadow looading cards

--- components/Dashboard/ClassGrid.tsx
+++ components/Dashboard/ClassGrid.tsx
@@ -3,13 +3,13 @@ import React from "react";
 import {useQuery} from "urql";
 import {allClasses} from "../../lib/graphql/Class";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import LoadingClassCard from "./LoadingClassCard";
 
 const LoadingPlaceholder = () => {
-    return (<div className="p-4">
-        <div className="mx-auto">
-            <div className="mx-auto w-full text-center"><CircularProgress color="secondary"/></div>
-        </div>
-    </div>)
+    return (<ul className="grid grid-cols-1 gap-4 sm:gap-6 sm:grid-cols-2 xl:grid-cols-2 mt-3">
+        <LoadingClassCard />
+        <LoadingClassCard />
+    </ul>)
 };
 
 interface Props {
