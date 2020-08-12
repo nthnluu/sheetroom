a116249ce3af2e46586ecca8a3d8cbbd82a79f49
Nathan Luu <nate1299@me.com>
2020-08-12 10:50:09 -0700

    Attempt #11 to fix DB

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -27,39 +27,38 @@ const options = {
         })
     ],
     // database: "postgres://rkofrjdyqoidnj:5e700ce4e559ae08a4306f70d66e203c9d6933b4afa5990f5766f31b26666c85@ec2-52-72-65-76.compute-1.amazonaws.com:5432/d2rnd6jboqu0mq"+"?sslmode=require",
-
-    adapter: Adapters.TypeORM.Adapter(
-        // The first argument should be a database connection string or TypeORM config object
-        {
-            type: 'postgres',
-            host: "ec2-52-72-65-76.compute-1.amazonaws.com",
-            port: 5432,
-            username: "rkofrjdyqoidnj",
-            password: "5e700ce4e559ae08a4306f70d66e203c9d6933b4afa5990f5766f31b26666c85",
-            database: "d2rnd6jboqu0mq",
-            synchronize: true,
-            ssl: {
-                rejectUnauthorized: false
-            }
-        },
-        // The second argument can be used to pass custom models and schemas
-        {
-            models: {
-                User: Models.User,
-            },
-        }
-    ),
+    // adapter: Adapters.TypeORM.Adapter(
+    //     // The first argument should be a database connection string or TypeORM config object
+    //     {
+    //         type: 'postgres',
+    //         host: "ec2-52-72-65-76.compute-1.amazonaws.com",
+    //         port: 5432,
+    //         username: "rkofrjdyqoidnj",
+    //         password: "5e700ce4e559ae08a4306f70d66e203c9d6933b4afa5990f5766f31b26666c85",
+    //         database: "d2rnd6jboqu0mq",
+    //         synchronize: true,
+    //         ssl: {
+    //             rejectUnauthorized: false
+    //         }
+    //     },
+    //     // The second argument can be used to pass custom models and schemas
+    //     {
+    //         models: {
+    //             User: Models.User,
+    //         },
+    //     }
+    // ),
     // A database is optional, but required to persist accounts in a database
-    // database: {
-    //     type: 'postgres',
-    //     host: "ec2-52-72-65-76.compute-1.amazonaws.com",
-    //     port: 5432,
-    //     username: "rkofrjdyqoidnj",
-    //     password: "5e700ce4e559ae08a4306f70d66e203c9d6933b4afa5990f5766f31b26666c85",
-    //     database: "d2rnd6jboqu0mq",
-    //     synchronize: false,
-    //     ssl: true
-    // },
+    database: {
+        type: 'postgres',
+        host: "ec2-52-72-65-76.compute-1.amazonaws.com",
+        port: 5432,
+        username: "rkofrjdyqoidnj",
+        password: "5e700ce4e559ae08a4306f70d66e203c9d6933b4afa5990f5766f31b26666c85",
+        database: "d2rnd6jboqu0mq",
+        synchronize: true,
+        ssl: true
+    },
     callbacks: {
         session: async (session: ISession, user: IUser) => {
             return Promise.resolve(user);
@@ -88,11 +87,6 @@ const options = {
             category: 'User',
             action: 'Signed in'
         }) },
-        signOut: async (message) => { /* on signout */ },
-        createUser: async (message) => { /* user created */ },
-        linkAccount: async (message) => { /* account linked to a user */ },
-        session: async (message) => { /* session is active */ },
-        error: async (message) => { /* error in authentication flow */ }
     }
 };
 
