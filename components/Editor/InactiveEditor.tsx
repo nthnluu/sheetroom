a65555160bbc3d24d6639b23df45a8b738fc4c06
Nathan Luu <nate1299@me.com>
2020-08-16 22:19:20 -0700

    Fixed typescript errors

--- components/Editor/InactiveEditor.tsx
+++ components/Editor/InactiveEditor.tsx
@@ -5,8 +5,6 @@ import {v4 as uuidv4} from 'uuid';
 const ReactQuill = typeof window === 'object' ? require('react-quill') : false;
 
 interface Props {
-    active: boolean;
-    onChange: any;
     value: object;
     placeholder?: string;
     border?: boolean;
@@ -14,26 +12,20 @@ interface Props {
 }
 
 
-const InactiveEditor: React.FC<Props> = ({active, value, onChange, placeholder, border}) => {
+const InactiveEditor: React.FC<Props> = ({value, placeholder, border}) => {
     const [isFocused, setIsFocused] = useState(false);
     const uniqueKey = useMemo(() => uuidv4(), []);
 
 
-
     return (<>
-        <div className={"font-sans text-gray-700 w-full z-40 pointer-events-none " + (border ? "border rounded-lg p-4 " : "p-0")}>
+        <div
+            className={"font-sans text-gray-700 w-full z-40 pointer-events-none " + (border ? "border rounded-lg p-4 " : "p-0")}>
             <ReactQuill
                 tabIndex={0}
                 key={"key" + uniqueKey}
                 id={uniqueKey + "xxxxx_myPropertyUsingQuill"}
-                onFocus={(() => {
-                    if (active) {
-                        setIsFocused(true)
-                    }
-                })}
-                onBlur={(() => setIsFocused(false))}
                 readOnly={true}
-                placeholder={placeholder} theme="bubble" value={value} onChange={(html) => onChange(html)}/>
+                placeholder={placeholder} theme="bubble" value={value}/>
         </div>
     </>)
 }
