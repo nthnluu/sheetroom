c61cd0617c56d586c0e7106e1bec31feae86fc79
Nathan Luu <nate1299@me.com>
2020-08-19 14:52:37 -0700

    Added mobilee graphic

--- components/LandingPage/DesktopGraphic.tsx
+++ components/LandingPage/DesktopGraphic.tsx
@@ -4,18 +4,18 @@ import React from "react";
 const DesktopGraphic = () => {
     return (<div className="relative">
 
-        <motion.picture className="absolute left-0 -ml-10 lg:-ml-20 top-0 lg:top-40 z-50" animate={{y: 14}}
+        <motion.picture className="absolute left-0 sm:-ml-8 lg:-ml-20 top-0 lg:top-60 z-50" animate={{y: 14}}
                         transition={{
                             yoyo: Infinity,
                             ease: "anticipate",
                             duration: 2
                         }} >
-            <source className="h-24 md:h-36 lg:h-64" srcSet="/landing_page/desktop/webp/Graph.webp" type="image/webp"/>
-            <source className="h-24 md:h-36 lg:h-64" srcSet="/landing_page/desktop/Graph.png" type="image/png"/>
-            <img className="h-24 md:h-36 lg:h-64" src="/landing_page/desktop/Graph.png"/>
+            <source className="h-24 sm:h-45 md:h-56 lg:h-64" srcSet="/landing_page/desktop/webp/Graph.webp" type="image/webp"/>
+            <source className="h-24 sm:h-45 md:h-56 lg:h-64" srcSet="/landing_page/desktop/Graph.png" type="image/png"/>
+            <img className="h-24 sm:h-45 md:h-56 lg:h-64" src="/landing_page/desktop/Graph.png"/>
         </motion.picture>
 
-        <motion.picture className="absolute right-0 -mr-10 lg:-mr-20 top-40 lg:top-80 z-50" animate={{y: -14}}
+        <motion.picture className="absolute right-0 -mr-4 lg:-mr-20 bottom-0 sm:top-80 z-50" animate={{y: -14}}
                         transition={{
                             yoyo: Infinity,
                             ease: "anticipate",
@@ -26,8 +26,20 @@ const DesktopGraphic = () => {
             <img className="h-24 md:h-36 lg:h-64" src="/landing_page/desktop/Image.png"/>
         </motion.picture>
 
-        <motion.picture className="absolute z-40"
-                        style={{opacity: 0.9, right: '23rem', bottom: '3.5rem'}} animate={{y: 14, x: -12, rotate: -10, opacity: 0.8}}
+        <motion.picture className="absolute z-40 block sm:hidden right-0"
+                        animate={{y: 14, x: -12, opacity: 0.7}}
+                        transition={{
+                            yoyo: Infinity,
+                            ease: "linear",
+                            duration: 4
+                        }} >
+            <source className="h-32 md:h-38 lg:h-40" srcSet="/landing_page/desktop/webp/Cube.webp" type="image/webp"/>
+            <source className="h-32 md:h-38 lg:h-40" srcSet="/landing_page/desktop/Cube.png" type="image/png"/>
+            <img className="h-32 md:h-38 lg:h-40" src="/landing_page/desktop/Cube.png"/>
+        </motion.picture>
+
+        <motion.picture className="absolute z-40 hidden sm:block"
+                        style={{opacity: 0.9, right: '25.5rem', bottom: '4.8rem'}} animate={{y: 14, x: -12, opacity: 0.8}}
                         transition={{
                             yoyo: Infinity,
                             ease: "linear",
@@ -39,24 +51,30 @@ const DesktopGraphic = () => {
         </motion.picture>
 
 
+
         <motion.picture animate={{scale: 0.9}}
                         transition={{
                             yoyo: Infinity,
                             ease: "linear",
                             duration: 3
                         }}
-                        className="absolute right-10 md:right-20 lg:right-40 top-5 md:top-20 z-40">
-            <source className="h-16 md:h-32 lg:h-40 " srcSet="/landing_page/desktop/webp/Sphere.webp" type="image/webp"/>
-            <source className="h-16 md:h-32 lg:h-40 " srcSet="/landing_page/desktop/Sphere.png" type="image/png"/>
-            <img className="h-16 md:h-32 lg:h-40 " src="/landing_page/desktop/Sphere.png"/>
+                        className="absolute right-10 md:right-20 lg:right-40 top-5 md:top-20 z-40 hidden sm:block">
+            <source className="h-32 lg:h-40 " srcSet="/landing_page/desktop/webp/Sphere.webp" type="image/webp"/>
+            <source className="h-32 lg:h-40 " srcSet="/landing_page/desktop/Sphere.png" type="image/png"/>
+            <img className="h-32 lg:h-40 " src="/landing_page/desktop/Sphere.png"/>
         </motion.picture>
 
-        <picture className="z-30 relative">
+        <picture className="z-30 relative block sm:hidden">
+            <source className="z-30 relative" srcSet="/landing_page/desktop/webp/Phone.webp" type="image/webp"/>
+            <source className="z-30 relative" srcSet="/landing_page/desktop/Phone.png" type="image/png"/>
+            <img  className="z-30 relative" src="/landing_page/desktop/Phone.png"/>
+        </picture>
+
+        <picture className="z-30 relative hidden sm:block">
             <source className="z-30 relative" srcSet="/landing_page/desktop/webp/Window.webp" type="image/webp"/>
             <source className="z-30 relative" srcSet="/landing_page/desktop/Window.png" type="image/png"/>
             <img  className="z-30 relative" src="/landing_page/desktop/Window.png"/>
         </picture>
-
 
         <motion.picture animate={{scale: 1.1}}
                         transition={{
@@ -64,10 +82,10 @@ const DesktopGraphic = () => {
                             ease: "anticipate",
                             duration: 3
                         }}
-                        className="absolute left-0 bottom-20 z-10">
-            <source className="h-24 md:h-36 lg:h-64 " srcSet="/landing_page/desktop/webp/BgSphere.webp" type="image/webp"/>
-            <source className="h-24 md:h-36 lg:h-64 " srcSet="/landing_page/desktop/BgSphere.png" type="image/png"/>
-            <img className="h-24 md:h-36 lg:h-64 " src="/landing_page/desktop/BgSphere.png"/>
+                        className="absolute left-0 bottom-0 sm:bottom-20 z-10">
+            <source className="h-32 md:h-36 lg:h-64 " srcSet="/landing_page/desktop/webp/BgSphere.webp" type="image/webp"/>
+            <source className="h-32 md:h-36 lg:h-64 " srcSet="/landing_page/desktop/BgSphere.png" type="image/png"/>
+            <img className="h-32 md:h-36 lg:h-64 " src="/landing_page/desktop/BgSphere.png"/>
         </motion.picture>
     </div>)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,14 @@ import DesktopGraphic from "../components/LandingPage/DesktopGraphic";
 const Index: React.FC<{ session: any }> = ({session}) => {
 
     return (
-        <div className="min-h-screen" style={{backgroundColor: '#E8F5FE'}}>
+        <div className="min-h-screen overflow-x-hidden" style={{backgroundColor: '#E8F5FE'}}>
             <div className="z-50">
                 <Navbar session={session} unfixed transparent darkText/>
             </div>
             <div>
-                <div className="mx-auto px-16 pb-12 pt-24 max-w-7xl">
-                    <header className="max-w-6xl mx-auto">
-                        <h1 className="text-4xl md:text-5xl lg:text-6xl text-center font-semibold text-gray-800 leading-tight z-40">Beautiful
+                <div className="mx-auto px-4 pb-12 pt-20 md:pt-24 max-w-7xl">
+                    <header className="max-w-6xl mx-auto mb-12 md:mb-0">
+                        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center font-semibold text-gray-800 leading-tight z-40">Beautiful
                             assignments for desktop, mobile and paper.
                         </h1>
                     </header>
