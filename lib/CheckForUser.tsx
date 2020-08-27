7b76b5b941701b51b1756caa256eb5d8c5fe9f29
Nathan Luu <nate1299@me.com>
2020-08-26 23:37:21 -0700

    alot

--- lib/CheckForUser.tsx
+++ lib/CheckForUser.tsx
@@ -5,7 +5,40 @@ const CheckForUser = async (req, res, privatePage = false) => {
     let session = await getSession({req});
     let profileData;
 
-    if (!session) {
+    if (session) {
+        if (session.id) {
+            profileData = await fetch('https://api.sheetroom.com/v1/graphql', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-hasura-admin-secret': 'HASURA_ADMIN_SECRETd92iecpo0@v#nfse-bflit!*@2*%xodd4dk6g(xra^nbxnc(a#PENIS'
+                },
+                body: JSON.stringify({query: me, variables: {userId: session.id}}),
+            })
+                .then(response => response.json())
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+            if (profileData.data.users_by_pk.account_type === "new") {
+                res.writeHead(302, {location: '/welcome'})
+                res.end()
+            } else {
+                return {
+                    props: {
+                        session,
+                        profileData
+                    },
+                };
+            }
+        } else {
+            return {
+                props: {
+                    session
+                },
+            };
+        }
+
+    } else {
         if (privatePage) {
             res.writeHead(302, {location: '/'})
             res.end()
@@ -13,30 +46,6 @@ const CheckForUser = async (req, res, privatePage = false) => {
             return {
                 props: {session}
             }
-        }
-    } else {
-        profileData = await fetch('https://api.sheetroom.com/v1/graphql', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-hasura-admin-secret': 'HASURA_ADMIN_SECRETd92iecpo0@v#nfse-bflit!*@2*%xodd4dk6g(xra^nbxnc(a#PENIS'
-            },
-            body: JSON.stringify({query: me, variables: {userId: session.id}}),
-        })
-            .then(response => response.json())
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-        if (profileData.data.users_by_pk.account_type === "new") {
-            res.writeHead(302, {location: '/welcome'})
-            res.end()
-        } else {
-            return {
-                props: {
-                    session,
-                    profileData
-                },
-            };
         }
     }
 }
