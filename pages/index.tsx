1e1eeccf56620afd88435b28e2e849858551b88e
Nathan Luu <nate1299@me.com>
2020-08-28 13:46:18 -0700

    Fixed SVG invalid props

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,8 +69,8 @@ const Index: React.FC<{ session: any }> = ({session}) => {
                                                         className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
                                                         <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                                                              stroke="currentColor">
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                  stroke-width="2"
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                  strokeWidth="2"
                                                                   d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"/>
                                                         </svg>
                                                     </div>
@@ -93,8 +93,8 @@ const Index: React.FC<{ session: any }> = ({session}) => {
                                                         className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
                                                         <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                                                              stroke="currentColor">
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                  stroke-width="2"
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                  strokeWidth="2"
                                                                   d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"/>
                                                         </svg>
                                                     </div>
@@ -117,8 +117,8 @@ const Index: React.FC<{ session: any }> = ({session}) => {
                                                         className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
                                                         <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                                                              stroke="currentColor">
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                  stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"/>
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                  strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"/>
                                                         </svg>
                                                     </div>
                                                 </div>
@@ -188,8 +188,8 @@ const Index: React.FC<{ session: any }> = ({session}) => {
                                                             className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
                                                             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                                                                  stroke="currentColor">
-                                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                                      stroke-width="2"
+                                                                <path strokeLinecap="round" strokeLinejoin="round"
+                                                                      strokeWidth="2"
                                                                       d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"/>
                                                             </svg>
                                                         </div>
@@ -212,8 +212,8 @@ const Index: React.FC<{ session: any }> = ({session}) => {
                                                             className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
                                                             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                                                                  stroke="currentColor">
-                                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                                      stroke-width="2"
+                                                                <path strokeLinecap="round" strokeLinejoin="round"
+                                                                      strokeWidth="2"
                                                                       d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
                                                             </svg>
                                                         </div>
