9c333d9809facbacaffa15d7ca52ab1c8b452865
Nathan Luu <nate1299@me.com>
2020-08-28 19:49:19 -0700

    Made it such that the "plus" add buttons are hidden when the account type isnt teacher

--- components/Dashboard/ProjectList.tsx
+++ components/Dashboard/ProjectList.tsx
@@ -4,6 +4,7 @@ import NewAssignmentModal from "../Modals/NewAssignmentModal";
 import NewClassModal from "../Modals/NewClassModal";
 import ClassGrid from "./ClassGrid";
 import NewTooltip from "../Misc/Tooltip";
+import JsonDebugBox from "../JsonDebugBox";
 
 
 interface Props {
@@ -31,8 +32,8 @@ const ProjectList: React.FC<Props> = ({session, profileData}) => {
                     <div className="flex items-center">
                         <h1 className="flex-1 text-lg leading-7 font-medium">Classes</h1>
                         <div>
-                            <NewTooltip title="New class" placement="bottom" enterDelay={500}
-                                        enterNextDelay={500}>
+                            {profileData.data.users_by_pk.account_type === "teacher" ? <NewTooltip title="New class" placement="bottom" enterDelay={500}
+                                                                                                   enterNextDelay={500}>
 
                                 <button type="button" onClick={() => toggleCreateClassDialog(true)}
                                         className="flex items-center justify-center h-8 w-8 border-gray-700 text-sm leading-5 font-medium rounded-md text-gray-700 bg-transparent hover:bg-gray-200 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
@@ -41,7 +42,7 @@ const ProjectList: React.FC<Props> = ({session, profileData}) => {
                                     </svg>
                                 </button>
 
-                            </NewTooltip>
+                            </NewTooltip> : null}
                         </div>
                     </div>
                 </div>
@@ -58,57 +59,8 @@ const ProjectList: React.FC<Props> = ({session, profileData}) => {
                     <h1 className="flex-1 text-lg leading-7 font-medium">Assignments</h1>
                     <div className="relative">
                         <div className="flex">
-                            {/*    <ClickAwayListener onClickAway={() => toggleSortDropdown(false)}>*/}
-                            {/*    <div>*/}
-                            {/*            <span className="rounded-md shadow-sm">*/}
-
-                            {/*    <button id="sort-menu" onClick={() => toggleSortDropdown(!sortDropdown)} type="button"*/}
-                            {/*            className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150"*/}
-                            {/*            aria-haspopup="true" aria-expanded="false">*/}
-                            {/*      <svg className="mr-3 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">*/}
-                            {/*        <path*/}
-                            {/*            d="M3 3a1 1 0 000 2h11a1 1 0 100-2H3zM3 7a1 1 0 000 2h5a1 1 0 000-2H3zM3 11a1 1 0 100 2h4a1 1 0 100-2H3zM13 16a1 1 0 102 0v-5.586l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 101.414 1.414L13 10.414V16z"/>*/}
-                            {/*      </svg>*/}
-                            {/*      Sort*/}
-                            {/*      <svg className="ml-2.5 -mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">*/}
-                            {/*        <path fillRule="evenodd"*/}
-                            {/*              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"*/}
-                            {/*              clipRule="evenodd"/>*/}
-                            {/*      </svg>*/}
-                            {/*    </button>*/}
-
-                            {/*  </span>*/}
-
-                            {/*        <Transition appear={sortDropdown} show={sortDropdown}*/}
-                            {/*                    enter="transition ease-out duration-100"*/}
-                            {/*                    enterFrom="transform opacity-0 scale-95"*/}
-                            {/*                    enterTo="transform opacity-100 scale-100"*/}
-                            {/*                    leave="transition ease-in duration-75"*/}
-                            {/*                    leaveTo="transform opacity-0 scale-95"*/}
-                            {/*                    leaveFrom="transform opacity-100 scale-100">*/}
-                            {/*            <div*/}
-                            {/*                className="origin-top-right z-10 absolute right-0 mt-2 w-56 rounded-md shadow-lg">*/}
-                            {/*                <div className="rounded-md bg-white shadow-xs">*/}
-                            {/*                    <div className="py-1" role="menu" aria-orientation="vertical"*/}
-                            {/*                         aria-labelledby="sort-menu">*/}
-                            {/*                        <a href="#"*/}
-                            {/*                           className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"*/}
-                            {/*                           role="menuitem">Name</a>*/}
-                            {/*                        <a href="#"*/}
-                            {/*                           className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"*/}
-                            {/*                           role="menuitem">Date modified</a>*/}
-                            {/*                        <a href="#"*/}
-                            {/*                           className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"*/}
-                            {/*                           role="menuitem">Date created</a>*/}
-                            {/*                    </div>*/}
-                            {/*                </div>*/}
-                            {/*            </div>*/}
-                            {/*        </Transition>*/}
-                            {/*    </div>*/}
-
-                            {/*</ClickAwayListener>*/}
-                            <NewTooltip title="New assignment" placement="bottom" enterDelay={500}
-                                        enterNextDelay={500}>
+                            {profileData.data.users_by_pk.account_type === "teacher" ?  <NewTooltip title="New assignment" placement="bottom" enterDelay={500}
+                                                                                                    enterNextDelay={500}>
 
                                 <button type="button" onClick={() => toggleCreateAssignmentDialog(true)}
                                         className="flex items-center justify-center h-8 w-8 border-gray-700 text-sm leading-5 font-medium rounded-md text-gray-700 bg-transparent hover:bg-gray-200 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
@@ -117,14 +69,15 @@ const ProjectList: React.FC<Props> = ({session, profileData}) => {
                                     </svg>
                                 </button>
 
-                            </NewTooltip>
+                            </NewTooltip> : null}
+
                         </div>
 
 
                     </div>
                 </div>
             </div>
-            <AssignmentList session={session} openDialog={() => toggleCreateAssignmentDialog(true)}/>
+            <AssignmentList profileData={profileData} session={session} openDialog={() => toggleCreateAssignmentDialog(true)}/>
         </div>)
 }
 
