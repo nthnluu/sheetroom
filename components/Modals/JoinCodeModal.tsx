d5b9a16c2246c7be814cb4b9bc52697fe221949d
Nathan Luu <nate1299@me.com>
2020-08-28 19:59:52 -0700

    Changed index bg to be a gradient, fixed spacing

--- components/Modals/JoinCodeModal.tsx
+++ components/Modals/JoinCodeModal.tsx
@@ -1,8 +1,5 @@
 import SimpleModal from "./SimpleModal";
-import React, {useContext} from "react";
-import QuizContext from "../AssignmentEditor/QuizContext";
-import update from "immutability-helper";
-import ToggleRow from "../Misc/ToggleRow";
+import React from "react";
 
 interface Props {
     joinCode: string;
@@ -25,11 +22,12 @@ const JoinCodeModal: React.FC<Props> = ({joinCode, title, onCancel, isOpen}) => 
                 className="inline-flex justify-center w-full rounded-md border border-transparent px-6 py-2 bg-blue-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
          Done
         </button>
+
       </span>
         </div>
         {/*@ts-ignore*/}
     </div>} isOpen={isOpen} onCancel={cancelModal} title={title} content={<div>
-      <h1 className="text-6xl text-center font-light text-gray-700 border border-gray-200 rounded-lg font-mono">{joinCode}</h1>
+      <h1 className="text-4xl md:text-5xl lg:text-6xl text-center font-light text-gray-700 border border-gray-200 rounded-lg font-mono">{joinCode}</h1>
 
     </div>}
     />)
