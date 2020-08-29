482ee3e8fca427ddc694687389d396ee41ade7c3
Nathan Luu <nate1299@me.com>
2020-08-28 20:27:19 -0700

    Added qr code button icon swithcing

--- components/Modals/JoinCodeModal.tsx
+++ components/Modals/JoinCodeModal.tsx
@@ -22,7 +22,7 @@ const JoinCodeModal: React.FC<Props> = ({joinCode, title, onCancel, isOpen}) => 
 
                         <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
                             <button onClick={() => toggleQrCode(!qrCode)} className="text-gray-600 border border-gray-300 shadow-sm px-3  rounded-lg mr-1 focus:outline-none focus:shadow-outline focus:border-blue-300">
-            <i className="fas fa-qrcode"/>
+            <i className={"fas " + (qrCode ? "fa-code" : "fa-qrcode")}/>
         </button>
         <button type="button" onClick={cancelModal}
                 className="inline-flex justify-center w-full rounded-md border border-transparent px-6 py-2 bg-blue-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
