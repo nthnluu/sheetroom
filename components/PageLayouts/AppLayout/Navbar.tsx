50d6c607f14c05102846cb2bed406a302ebfe06a
Nathan Luu <nate1299@me.com>
2020-08-28 22:45:58 -0700

    Fixed links on navbar and made them adapt to account type

--- components/PageLayouts/AppLayout/Navbar.tsx
+++ components/PageLayouts/AppLayout/Navbar.tsx
@@ -125,9 +125,10 @@ export const Navbar: React.FC<Props> = ({session, profileData, unfixed, transpar
                                 <button onClick={() => toggleFeedbackModal(true)}
                                         className="px-3 py-2 rounded-md text-sm leading-5 font-medium hover:text-white focus:outline-none focus:text-white focus:bg-light transition duration-150 ease-in-out">Feedback
                                 </button>
-                                <a href="#"
-                                   className="px-3 py-2 rounded-md text-sm leading-5 font-medium hover:text-white focus:outline-none focus:text-white focus:bg-light transition duration-150 ease-in-out">Learn</a>
-                                <ClickAwayListener onClickAway={() => toggleNewDropdown(false)}>
+                                {accountMode === "teacher" ? <a href="#"
+                                                                className="px-3 py-2 rounded-md text-sm leading-5 font-medium hover:text-white focus:outline-none focus:text-white focus:bg-light transition duration-150 ease-in-out">Help</a> : <a href="/join"
+                                                          className="px-3 py-2 mr-3 rounded-md text-sm leading-5 font-medium hover:text-white focus:outline-none focus:text-white focus:bg-light transition duration-150 ease-in-out">Join</a>}
+                                {accountMode === "teacher" ? <ClickAwayListener onClickAway={() => toggleNewDropdown(false)}>
                                     <div className="relative inline-block text-left mr-6 ml-4">
                                         <div>
                                     <span className="rounded-md shadow-sm">
@@ -171,7 +172,8 @@ export const Navbar: React.FC<Props> = ({session, profileData, unfixed, transpar
                                             </div>
                                         </Transition>
                                     </div>
-                                </ClickAwayListener>
+                                </ClickAwayListener> : null}
+
                                 <ClickAwayListener onClickAway={() => toggleProfileDropdown(false)}>
                                     <div className="relative flex-shrink-0">
                                         <div>
@@ -250,6 +252,18 @@ export const Navbar: React.FC<Props> = ({session, profileData, unfixed, transpar
                                             <span>Dashboard</span>
                                         </a>
                                     </li>
+                                    {accountMode === "teacher" ? <li className="flow-root">
+                                        <a href="/"
+                                           className="-m-3 p-3 flex items-center space-x-4 rounded-md text-base leading-6 font-medium text-gray-900 hover:bg-gray-50 transition ease-in-out duration-150">
+                                            <svg className="flex-shrink-0 h-6 w-6 text-gray-400" fill="none"
+                                                 viewBox="0 0 24 24" stroke="currentColor">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                                      d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"/>
+                                            </svg>
+                                            <span>Help</span>
+                                        </a>
+                                    </li> : null}
+
                                     <li className="flow-root">
                                         <button onClick={() => {toggleMobileMenu(false); toggleFeedbackModal(true)}}
                                            className="-m-3 p-3 w-full text-left flex items-center space-x-4 rounded-md text-base leading-6 font-medium text-gray-900 hover:bg-gray-50 transition ease-in-out duration-150">
