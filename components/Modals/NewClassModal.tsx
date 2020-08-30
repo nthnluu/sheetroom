1add73ddccffea915039a2be3d747ee24532fdcf
Nathan Luu <nate1299@me.com>
2020-08-29 22:49:05 -0700

    Added cancel button to delete modal

--- components/Modals/NewClassModal.tsx
+++ components/Modals/NewClassModal.tsx
@@ -4,11 +4,13 @@ import {useMutation} from "urql";
 import ReactGA from "react-ga";
 import {createClass} from "../../lib/graphql/Class";
 import {nanoid} from "nanoid";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 
 const NewClassModal = ({isOpen, onCancel, session}) => {
     const [createClassResult, createNewClass] = useMutation(createClass);
     const [currentValue, setNewValue] = useState("Untitled Class");
+    const [isLoading, toggleLoading] = useState(false)
 
     const colors = ["teal", "green", "red", "pink", "orange", "purple", "red"]
 
@@ -46,8 +48,8 @@ const NewClassModal = ({isOpen, onCancel, session}) => {
                 }));
         }}
             // @ts-ignore
-                disabled={currentValue.length === 0} className={"inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 text-base leading-6 font-medium text-white shadow-sm  transition ease-in-out duration-150 sm:text-sm sm:leading-5 " + (currentValue.length === 0 ? "bg-blue-400" : "bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue")}>
-          Create
+                disabled={currentValue.length === 0 || isLoading} className={"inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 text-base leading-6 font-medium text-white shadow-sm  transition ease-in-out duration-150 sm:text-sm sm:leading-5 " + (currentValue.length === 0 ? "bg-blue-400" : "bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue")}>
+          {isLoading ? <CircularProgress color="inherit" size={15} className="mr-2 h-auto"/> : null}Create
         </button>
       </span>
             <span className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
