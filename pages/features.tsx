3da8a66aa5c594ad3d3a5a9d768dbe74f7cc3e25
Nathan Luu <nate1299@me.com>
2020-09-03 22:04:35 -0700

    Added CTA to bottom of feature page

--- pages/features.tsx
+++ pages/features.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/PageLayouts/AppLayout/Navbar";
 import Footer from "../components/Misc/Footer";
+import React from "react";
 
 const Features = () => {
 
@@ -298,6 +299,32 @@ const Features = () => {
                 </svg>
             </div>
         </div>
+        <section>
+            <div className="bg-gray-50">
+                <div
+                    className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+                    <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+                        Ready to dive in?
+                        <br/>
+                        <span className="text-blue-600">Start for free today.</span>
+                    </h2>
+                    <div className="mt-8 flex lg:flex-shrink-0 lg:mt-0">
+                        <div className="inline-flex rounded-md shadow">
+                            <a href="/signin"
+                               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                                Get started
+                            </a>
+                        </div>
+                        <div className="ml-3 inline-flex rounded-md shadow">
+                            <a href="/features"
+                               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-blue-600 bg-white hover:text-blue-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                                See how it works
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         <Footer/>
     </>
 }
