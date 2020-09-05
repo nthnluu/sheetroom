11b9de7b9d303ecb7f0c518de4bc85b2b6a1cf12
Nathan Luu <nate1299@me.com>
2020-09-05 15:11:31 -0700

    Removed overflow y auto to invite settings

--- components/Modals/InviteSettings.tsx
+++ components/Modals/InviteSettings.tsx
@@ -94,7 +94,7 @@ const InviteSettings = ({isPublic, selectedClass, setSelectedClass, settingsObje
     const userIsPro = profileData.data.users_by_pk.is_pro
 
 
-    return (<div className="w-full h-48 overflow-y-auto">
+    return (<div className="w-full">
         <Tabs activeTab={currentTab} setActiveTab={index => setCurrentTab(index)}
               tabs={["General", "Visibility", "Advanced"]}/>
         {currentTab === 0 ? <>
